f3e2bd677c89f3ac97901175e9eff92a5264238b
Gislaine-wimora <massohgislaine@gmail.com>
2023-08-07 16:24:28 -0500

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,4 @@ images=`docker images | grep kammana/nodejenkins | awk '{print $3}'`
 docker rmi $images
 docker run -d -p 8080:8080 --name nodeapp $1
 # this is the deploy.sh  file
+#welcome to gis DevOps file
